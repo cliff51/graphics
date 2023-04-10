bb5fec25e4fc08f156766e8a48024a3259ff2eac
cliff51 <130289883+cliff51@users.noreply.github.com>
2023-04-11 08:26:12 +0900

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+#include "shader.h"
+#include "cube.h"
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+#endif // MAIN_H
